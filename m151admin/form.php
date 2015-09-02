7eae80f3edd1bb4e7d905ee3fa837705cfe0f355
miroslavtchakalov <miroslavtchakalov@yahoo.fr>
2015-09-02 13:16:23 +0200

    Labels et Connexion

--- m151admin/form.php
+++ m151admin/form.php
@@ -1,7 +1,7 @@
+<!DOCTYPE html>
 <?php
  require('Connexion.php');
 ?>
-<!DOCTYPE html>
 <html>
     <head>
         <title>form</title>
@@ -13,13 +13,13 @@
         <div id="Container">
             <form action="form.html" method="POST">
                 
-                <div class="divItem">Prenom : <input type="text" placeholder="Prenom" name="FirstName" class="item"></div>
-                <div class="divItem">Nom : <input type="text" placeholder="Nom de Famille" name="LastName" class="item"></div>
-                <div class="divItem">Date de naissance :<input type="date" name="BirthDate" class="item"></div>
-                <div class="divItem">Description :<textarea class="item"></textarea></div>
-                <div class="divItem">Email :<input type="email" name="Email" class="item"></div>
-                <div class="divItem">Pseudo : <input type="text" name="Nickname" class="item"></div>
-                <div class="divItem">Mot de passe : <input type="password" name="Password" class="item"></div>
+                <label class="divItem" for="FN">Prenom : </label><input type="text" placeholder="Prenom" name="FirstName" class="item" id="FN">
+                <label class="divItem" for="LN">Nom : </label><input type="text" placeholder="Nom de Famille" name="LastName" class="item" id="LN">
+                <label class="divItem" for="birth">Date de naissance :</label><input type="date" name="BirthDate" class="item" id="birth">
+                <label class="divItem" for="textarea">Description :</label><textarea class="item" id="textarea"></textarea>
+                <label class="divItem" for="email">Email :</label><input type="email" name="Email" class="item" id="email">
+                <label class="divItem" for="Pseudo">Pseudo : </label><input type="text" name="Nickname" class="item" id="Pseudo">
+                <label class="divItem" for="pass">Mot de passe : </label><input type="password" name="Password" class="item" id="pass">
                 <input type="submit" name="Validate" value="Valider" class="bouttons">  
                 <input type="button" name="Delete" value="Effacer" class="bouttons">         
                 
