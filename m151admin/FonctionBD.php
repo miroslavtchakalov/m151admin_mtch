66387df9f817e9695a7fbcec2dc5b7195de18b67
miroslavtchakalov <miroslavtchakalov@yahoo.fr>
2015-09-23 16:03:16 +0200

    GetDetails suite

--- m151admin/FonctionBD.php
+++ m151admin/FonctionBD.php
@@ -1,48 +1,58 @@
 
-        <?php
-        define("HOST", "127.0.0.1");
-        define("DBNAME", "m151ex1");
-        define("USER", "miroslav");
-        define("PASS", "Super");
+<?php
 
-        function GetConnexion() {
-            static $dbh = NULL;
-            if ($dbh === NULL) {
-                try {
-                    $dbh = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASS);
-                } catch (PDOException $e) {
-                    print "Erreur !: " . $e->getMessage() . "<br/>";
-                }
-            }
+define("HOST", "127.0.0.1");
+define("DBNAME", "m151ex1");
+define("USER", "miroslav");
+define("PASS", "Super");
 
-            return $dbh;
+function GetConnexion() {
+    static $dbh = NULL;
+    if ($dbh === NULL) {
+        try {
+            $dbh = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASS);
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
         }
+    }
 
-        function Insertion() {
-            $db = GetConnexion();
-            $req = $db->prepare('INSERT INTO users(prenom,nom,dateNaissance,description,email,pseudo,motPass) VALUES(:FirstName, :LastName, :BirthDate, :Description, :Email,:Nickname, :Password)');
-            $req->bindParam(':FirstName', $_POST['FirstName'], PDO::PARAM_STR);
-            $req->bindParam(':LastName', $_POST['LastName'], PDO::PARAM_STR);
-            $req->bindParam(':BirthDate', $_POST['BirthDate'], PDO::PARAM_STR);
-            $req->bindParam(':Description', $_POST['Description'], PDO::PARAM_STR);
-            $req->bindParam(':Email', $_POST['Email'], PDO::PARAM_STR);
-            $req->bindParam(':Nickname', $_POST['Nickname'], PDO::PARAM_STR);
-            $req->bindParam(':Password', $_POST['Password'], PDO::PARAM_STR);
-            $retour = $req->execute();
-        }
+    return $dbh;
+}
 
-        function SelectUsers() {
-            $db = GetConnexion();
-            $req = $db->prepare("SELECT * from users");
-            $req->execute();
-            $requestResponse = $req->fetchAll(PDO::PARAM_STR);
-            return $requestResponse;
-        }
+function Insertion() {
+    $db = GetConnexion();
+    $req = $db->prepare('INSERT INTO users(prenom,nom,dateNaissance,description,email,pseudo,motPass) VALUES(:FirstName, :LastName, :BirthDate, :Description, :Email,:Nickname, :Password)');
+    $req->bindParam(':FirstName', $_POST['FirstName'], PDO::PARAM_STR);
+    $req->bindParam(':LastName', $_POST['LastName'], PDO::PARAM_STR);
+    $req->bindParam(':BirthDate', $_POST['BirthDate'], PDO::PARAM_STR);
+    $req->bindParam(':Description', $_POST['Description'], PDO::PARAM_STR);
+    $req->bindParam(':Email', $_POST['Email'], PDO::PARAM_STR);
+    $req->bindParam(':Nickname', $_POST['Nickname'], PDO::PARAM_STR);
+    $req->bindParam(':Password', $_POST['Password'], PDO::PARAM_STR);
+    $retour = $req->execute();
+}
 
-        function AssocToHtml($listUsers) {
-            foreach ($listUsers as $val) {
-                echo '<tr><td>' . $val['prenom'] . ' </td><td> ' . $val['nom'] . '</td><td> <a href="Userdetail.php"> <= voir les details</a></td></tr>';
-            }
-        }
-        ?>
- 
+function SelectUsers() {
+    $db = GetConnexion();
+    $req = $db->prepare("SELECT prenom,nom from users");
+    $req->execute();
+    $requestResponse = $req->fetchAll(PDO::PARAM_STR);
+    return $requestResponse;
+}
+function SelectUser() {
+    $db = GetConnexion();
+    $req = $db->prepare("SELECT prenom,nom from users");
+    $req->execute();
+    $requestResponse = $req->fetchAll(PDO::PARAM_STR);
+    return $requestResponse;
+}
+
+function AssocToHtml($listUsers) {
+    foreach ($listUsers as $val) {
+        echo '<tr><td>' . $val['prenom'] . ' </td><td> ' . $val['nom'] . '</td><td> <a href="Userdetail.php"> <= voir les details</a></td></tr>';
+    }
+}
+function DetailsToHtml($UserInfo){
+   
+    }
+}
--- m151admin/UserDetail.php
+++ m151admin/UserDetail.php
@@ -1,8 +1,23 @@
+
 <?php
 include './FonctionBD.php';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" href="Stylesheet.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="Container">
+            <table>
+            <?php
+            AssocToHtml(SelectUsers());
+            ?>
+            </table>
+        </div>
+    </body>
+</html>
 
