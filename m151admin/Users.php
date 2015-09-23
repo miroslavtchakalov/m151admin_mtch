23f49b1fd37dadc724278dd4f893a44adb328f26
miroslavtchakalov <miroslavtchakalov@yahoo.fr>
2015-09-23 14:43:26 +0200

    fin du Read

--- m151admin/Users.php
+++ m151admin/Users.php
@@ -1,9 +1,20 @@
 <?php
 include './FonctionBD.php';
-AssocToHtml(SelectUsers());
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" href="Stylesheet.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="Container">
+            <?php
+            AssocToHtml(SelectUsers());
+            ?>
+        </div>
+    </body>
+</html>
 
--- m151admin/form.php
+++ m151admin/form.php
@@ -19,16 +19,16 @@
         <div id="Container">
             <form action=form.php method="POST">
                 
-                <label class="divItem" for="FN">Prenom : </label><input type="text" placeholder="Prenom" name="FirstName" class="item" id="FN"></br>
-                <label class="divItem" for="LN">Nom : </label><input type="text" placeholder="Nom de Famille" name="LastName" class="item" id="LN"></br>
-                <label class="divItem" for="birth">Date de naissance :</label><input type="date" name="BirthDate" class="item" id="birth"></br>
-                <label class="divItem" for="textarea">Description :</label><textarea class="item" name="description" id="textarea"></textarea></br>
-                <label class="divItem" for="email">Email :</label><input type="email" name="Email" class="item" id="email"></br>
-                <label class="divItem" for="Pseudo">Pseudo : </label><input type="text" name="Nickname" class="item" id="Pseudo"></br>
-                <label class="divItem" for="pass">Mot de passe : </label><input type="password" name="Password" class="item" id="pass"></br>
+                <label class="divItem" for="FN">Prenom : </label><input type="text" placeholder="Prenom" name="FirstName" class="item" id="FN">
+                <label class="divItem" for="LN">Nom : </label><input type="text" placeholder="Nom de Famille" name="LastName" class="item" id="LN">
+                <label class="divItem" for="birth">Date de naissance :</label><input type="date" name="BirthDate" class="item" id="birth">
+                <label class="divItem" for="textarea">Description :</label><textarea class="item" name="description" id="textarea"></textarea>
+                <label class="divItem" for="email">Email :</label><input type="email" name="Email" class="item" id="email">
+                <label class="divItem" for="Pseudo">Pseudo : </label><input type="text" name="Nickname" class="item" id="Pseudo">
+                <label class="divItem" for="pass">Mot de passe : </label><input type="password" name="Password" class="item" id="pass">
                 <input type="submit" name="Validate" value="Valider" class="bouttons">  
                 <input type="button" name="Delete" value="Effacer" class="bouttons">         
-                
+                <a href="Users.php"> Lister les utilisateurs</a>
             </form>
         </div>
     </body>
